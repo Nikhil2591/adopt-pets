7fa425a03285f704b7f897d67e21341d756f8dee
Nikhil Shah <Nikhil_369@hotmail.co.uk>
2020-06-09 11:33:39 +0100

    Initial commit

---
+++ src/views/Pet.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>{{ animal.name }} ({{ $route.params.species }})</h1>
+        <h1>Age: {{ animal.age }} years old</h1>
+        <h1>Breed: {{ animal.breed }}</h1>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+
+export default {
+    data() {
+        return {
+            animal: {}
+        }
+    },
+    computed: {
+        ...mapState([
+            'cats',
+            'dogs',
+        ])
+    },
+    mounted() {
+            const animal = this[this.$route.params.species][this.$route.params.id]
+            return animal
+        }
+    }
+</script>
